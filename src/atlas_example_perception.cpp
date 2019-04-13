19755d925c6ead36c8ca6ad8272f436131ce7ec2
dpwivagg <dpwivagg@gmail.com>
2019-04-12 22:37:48 -0400

    Add starter nodes

---
+++ src/atlas_example_perception.cpp
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <std_msgs/String.h>
+#include <tough_perception_common/MultisenseImage.h>
+#include <opencv2/core.hpp>
+#include <opencv/highgui.h>
+
+void showImage(cv::Mat &image,std::string name)
+{
+    cv::namedWindow(name,cv::WINDOW_AUTOSIZE);
+    cv::imshow(name,image);
+    ROS_INFO("Press ESC or q to continue");
+    while(cv::waitKey(1)!=27 && cv::waitKey(1)!='q');
+    ROS_INFO("closing window");
+    cv::destroyWindow(name); // close that window
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "test_multisense_image"); // Initialize ROS node named test_multisense_image
+  ros::NodeHandle nh; // Create node handle so that all libraries are in the same node
+  bool status;
+
+  tough_perception::MultisenseImage imageHandler(nh);
+ 
+  cv::Mat image, gray, blur, edges, abs_edges;
+  status = imageHandler.giveImage(image);
+    
+  ROS_INFO_STREAM("[Height]" << imageHandler.giveHeight() << " [width]" <<imageHandler.giveWidth());
+  ROS_INFO("image status %s",status ? "true" : "false");
+
+  if(status) {
+    showImage(image,"RGB Image");
+
+    cv::cvtColor( image, gray, cv::COLOR_BGR2GRAY ); // Convert image to grayscale
+    showImage( gray, "Grayscale Image");
+
+    cv::GaussianBlur( gray, blur, cv::Size(3, 3), 0, 0, cv::BORDER_DEFAULT ); // Blur
+    showImage( blur, "Blurred Image");
+
+    cv::Laplacian( blur, edges, CV_16S, 3, 1, 0, cv::BORDER_DEFAULT ); // Compute Laplacian
+    cv::convertScaleAbs( edges, abs_edges );
+    showImage( abs_edges, "Edges");
+  }
+
+  return 0;
+}
---
+++ src/talker.cpp
@@ -0,0 +1,21 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "talker");
+    ros::NodeHandle nh;
+    ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
+
+    ros::Rate loop_rate(10);
+
+    while(ros::ok()) {
+        std_msgs::String msg;
+        msg.data = "hello";
+
+        chatter_pub.publish(msg);
+
+        ros::spinOnce();
+    }
+
+    return 0;
+}
