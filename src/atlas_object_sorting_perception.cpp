148350427763116399110fb8989ffdd0b35a27a0
dpwivagg <dpwivagg@gmail.com>
2019-04-17 10:00:26 -0400

    Update with RGBD methods

--- src/atlas_object_sorting_perception.cpp
+++ src/atlas_object_sorting_perception.cpp
@@ -1,41 +1,106 @@
+#include <ros/ros.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <std_msgs/String.h>
-#include <tough_perception_common/MultisenseImage.h>
+#include <tough_perception_common/MultisenseImageInterface.h>
 #include <tough_perception_common/MultisensePointCloud.h>
 #include <opencv2/core.hpp>
 #include <opencv/highgui.h>
+#include <pcl/point_types.h>
 
-void showImage(cv::Mat &image,std::string name)
-{
-    cv::namedWindow(name,cv::WINDOW_AUTOSIZE);
-    cv::imshow(name,image);
-    ROS_INFO("Press ESC or q to continue");
-    while(cv::waitKey(1)!=27 && cv::waitKey(1)!='q');
-    ROS_INFO("closing window");
-    cv::destroyWindow(name); // close that window
+void showImage(cv::Mat &image,std::string name) {
+  cv::namedWindow(name,cv::WINDOW_AUTOSIZE);
+  cv::imshow(name,image);
+  ROS_INFO("Press ESC or q to continue");
+  while(cv::waitKey(1)!=27 && cv::waitKey(1)!='q');
+  ROS_INFO("closing window");
+  cv::destroyWindow(name); // close that window
 }
 
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "test_multisense_image"); // Initialize ROS node named test_multisense_image
+void printStatus(const std::string& image_name, bool status) {
+  ROS_INFO("%s status %s", image_name.c_str(), status ? "true" : "false");
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "atlas_object_sorting_perception"); // Initialize ROS node named test_multisense_image
   ros::NodeHandle nh; // Create node handle so that all libraries are in the same node
+
+  ros::AsyncSpinner spinner(2);
+  spinner.start();
+
+  ros::Publisher pcPub = nh.advertise<sensor_msgs::PointCloud2>("stereoPointCloud", 1000);
+
   bool status, pcStatus;
 
-  tough_perception::MultisenseImage imageHandler(nh);
-  tough_perception::MultisensePointCloud pcHandler(nh, "/head", "/left_camera_optical_frame");
+  tough_perception::MultisenseImageInterfacePtr imageHandler;
+  imageHandler = tough_perception::MultisenseImageInterface::getMultisenseImageInterface(nh);
+
+  tough_perception::MultisenseCameraModel cam_model;
+  imageHandler->getCameraInfo(cam_model);
 
   cv::Mat image, blueMask, redMask, greenMask;
-  status = imageHandler.giveImage(image);
+  cv::Mat color;
+  cv::Mat_<float> disp;
+
+  cv::Mat depthImage;
+
+  bool new_color = false;
+  bool new_disp = false;
+
+  ros::Publisher debug_publisher = nh.advertise<pcl::PCLPointCloud2>("/debug/RGBD", 1);
+
+  tough_perception::StereoPointCloudColor::Ptr organized_cloud(new tough_perception::StereoPointCloudColor);
+
+  if (imageHandler->getImage(color))
+  {
+    new_color = true;
+  }
+  ROS_INFO("image status %s",new_color ? "true" : "false");
+  if (imageHandler->getDisparity(disp, true))
+  {
+    new_disp = true;
+  }
+  ROS_INFO("disparity status %s",new_disp ? "true" : "false");
+  
+  if (new_disp && new_color)
+  {
+    std::cout << disp.cols << " x " << disp.rows << std::endl;
+    tough_perception::generateOrganizedRGBDCloud(disp, color, cam_model.Q, organized_cloud);
+    std::cout << "Post point cloud organizing" << std::endl;
+    ROS_INFO_STREAM("Organized cloud size: " << organized_cloud->size());
+    pcl::PCLPointCloud2 output;
+    pcl::toPCLPointCloud2(*organized_cloud, output);
+    output.header.frame_id = std::string("world");
+    output.header.stamp = ros::Time::now().toNSec();
+    debug_publisher.publish(output);
+
+    new_disp = new_color = false;
+  }
+
+  spinner.stop();
+
+// *************************************************************
+// Image color filtering code below
+// *************************************************************
+/*
+  status = imageHandler->getImage(image);
 
   // Get a point cloud from the stereo vision system
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
+  sensor_msgs::PointCloud2 rosCloud;
   // tough_perception::StereoPointCloud cloud;
-  // pcStatus = pcHandler.giveStereoCloud(cloud);
-    
+  pcStatus = pcHandler.giveStereoCloud(cloud);
+
+  ROS_INFO("point cloud status %s",pcStatus ? "true" : "false");
+  if(pcStatus) {
+    pcl::toROSMsg(*cloud, rosCloud);
+    pcPub.publish(rosCloud);
+  }
+  
   ROS_INFO_STREAM("[Height]" << imageHandler.giveHeight() << " [width]" <<imageHandler.giveWidth());
   ROS_INFO("image status %s",status ? "true" : "false");
 
-  if(status) {
+  if(1 == 0) {
     showImage(image,"BGR Image");
 
     // Convert the images to binary color threshold images using
@@ -55,6 +120,7 @@ int main(int argc, char **argv)
     // Use https://www.learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/
 
     cv::Moments blueMoments = moments(blueMask, true);
+
     cv::Point p(blueMoments.m10/blueMoments.m00, blueMoments.m01/blueMoments.m00);
 
     // Plot a red circle on the blue centroid for visualization purposes
@@ -63,6 +129,6 @@ int main(int argc, char **argv)
 
     // TODO: Use LIDAR or stereo to detect depth for determining 3D location for walking target
   }
-
+*/
   return 0;
 }
