5089d07b812de5bebe47a2c760027393d7a6045c
Shannon Marija Moffat <smmoffat@ace-viz01.wpi.edu>
2019-04-24 21:46:41 -0400

    feat: Edit atlas_object_sorting_navigation to create RobotWalkToGoal class

--- src/atlas_object_sorting_navigation.cpp
+++ src/atlas_object_sorting_navigation.cpp
@@ -2,26 +2,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <std_msgs/String.h>
-#include <touch_footstep/robot_walker.h>
+#include <tough_footstep/robot_walker.h>
 #include "geometry_msgs/Pose2D.h"
 
+class RobotWalkToGoal {
 
-void atlas_walk_to_goal(const geometry_msgs::Pose2D& goal) {
-  RobotWalker walk(nh, 1.0, 1.0, 0);
-  std::string msg = "Walking to x: " + std::to_string(goal.x) + ", y: " + std::to_string(goal.y) +
+public:
+  RobotWalkToGoal() {
+    sub_ = n_.subscribe("atlas_object_sorting_perception", 1000, &RobotWalkToGoal::callback, this);
+  }
+
+  void callback(const geometry_msgs::Pose2D& goal) {
+    RobotWalker walk(n_, 1.0, 1.0, 0);
+    std::string msg = "Walking to x: " + std::to_string(goal.x) + ", y: " + std::to_string(goal.y) +
                       ", theta: " + std::to_string(goal.theta);
-  ROS_INFO("%s", msg.c_str());
-  walk.walkToGoal(goal);
-}
+    ROS_INFO("%s", msg.c_str());
+    walk.walkToGoal(goal);
+  }
+
+private:
+  ros::NodeHandle n_;
+  ros::Subscriber sub_;
+
+};
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "atlas_object_sorting_navigation"); // Initialize ROS node
-  ros::NodeHandle nh; // Create node handle so that all libraries are in the same node
-
+  
   ros::AsyncSpinner spinner(2);
   spinner.start();
 
-  ros::Subscriber nav_goal_sub = nh.subscribe("atlas_object_sorting_perception", 1000, atlas_walk_to_goal);
+  RobotWalkToGoal atlas;
 
   spinner.stop();
 
