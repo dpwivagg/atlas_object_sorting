a9dbf7814c394070b0beb605e3726fd21eb4506f
dpwivagg <dpwivagg@gmail.com>
2019-04-26 17:24:44 -0400

    Update perception node to find centroid of blue points

--- src/atlas_object_sorting_perception.cpp
+++ src/atlas_object_sorting_perception.cpp
@@ -9,19 +9,10 @@
 #include <pcl/point_types.h>
 #include <pcl_ros/transforms.h>
 #include "sensor_msgs/PointCloud2.h"
+#include "geometry_msgs/Pose2D.h"
+#include "geometry_msgs/PoseStamped.h"
 
-void showImage(cv::Mat &image,std::string name) {
-  cv::namedWindow(name,cv::WINDOW_AUTOSIZE);
-  cv::imshow(name,image);
-  ROS_INFO("Press ESC or q to continue");
-  while(cv::waitKey(1)!=27 && cv::waitKey(1)!='q');
-  ROS_INFO("closing window");
-  cv::destroyWindow(name); // close that window
-}
-
-void printStatus(const std::string& image_name, bool status) {
-  ROS_INFO("%s status %s", image_name.c_str(), status ? "true" : "false");
-}
+typedef pcl::PointXYZRGB PointT;
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "atlas_object_sorting_perception"); // Initialize ROS node named test_multisense_image
@@ -31,33 +22,24 @@ int main(int argc, char **argv) {
   spinner.start();
 
   ros::Publisher pcPub = nh.advertise<sensor_msgs::PointCloud2>("stereoPointCloud", 1000);
-  ros::Publisher debug_publisher = nh.advertise<pcl::PCLPointCloud2>("/debug/RGBDTest", 1);
+  ros::Publisher debug_publisher = nh.advertise<pcl::PCLPointCloud2>("/debug/RGBD", 1);
+  ros::Publisher pose_pub = nh.advertise<geometry_msgs::Pose2D>("walkingGoal", 1);
+  ros::Publisher pose_pub_rviz = nh.advertise<geometry_msgs::PoseStamped>("walkingGoalRviz", 1);
 
-  bool status, pcStatus;
-
-  ROS_INFO("Hello, world!");
   tough_perception::MultisenseImageInterfacePtr imageHandler;
   imageHandler = tough_perception::MultisenseImageInterface::getMultisenseImageInterface(nh);
 
-  ROS_INFO("Interface started");
-
   tough_perception::MultisenseCameraModel cam_model;
   imageHandler->getCameraInfo(cam_model);
-
-  cv::Mat image, blueMask, redMask, greenMask;
+  
   cv::Mat color;
   cv::Mat_<float> disp;
-
-  cv::Mat depthImage;
 
   bool new_color = false;
   bool new_disp = false;
 
-  ROS_INFO("Grabbing organized point cloud");
-
   tough_perception::StereoPointCloudColor::Ptr organized_cloud(new tough_perception::StereoPointCloudColor);
 
-  ROS_INFO("Point cloud recieved");
   if (imageHandler->getImage(color))
   {
     new_color = true;
@@ -71,120 +53,46 @@ int main(int argc, char **argv) {
   
   if (new_disp && new_color)
   {
-    std::cout << disp.cols << " x " << disp.rows << std::endl;
     tough_perception::generateOrganizedRGBDCloud(disp, color, cam_model.Q, organized_cloud);
-    std::cout << "Post point cloud organizing" << std::endl;
     ROS_INFO_STREAM("Organized cloud size: " << organized_cloud->size());
     
-    pcl::PCLPointCloud2 output;
-    pcl::toPCLPointCloud2(*organized_cloud, output);
-    sensor_msgs::PointCloud2 outputRos;
-    pcl::toROSMsg(*organized_cloud, outputRos);
     const std::string leftOptFrame = "left_camera_optical_frame";
-    outputRos.header.frame_id = leftOptFrame;
-    outputRos.header.stamp = ros::Time::now();
-    debug_publisher.publish(outputRos);
-    ROS_INFO("Cloud published to /debug/RGBDTest");
     new_disp = new_color = false;
 
-    if(!organized_cloud->empty()) {
-      // Generate and fill cv Mats for processing
-      cv::Mat image(organized_cloud->height, organized_cloud->width, CV_8UC4);
+    ROS_INFO("Computing color-based centroids");
 
-      #pragma omp parallel for
-      for(int y = 0; y < image.rows; y++) {
-        for(int x = 0; x < image.cols; x++) {
-          pcl::PointXYZRGB point = organized_cloud->at(x, y);
-          image.at<cv::Vec4b>(y, x)[0] = point.b;
-          image.at<cv::Vec4b>(y, x)[1] = point.g;
-          image.at<cv::Vec4b>(y, x)[2] = point.r;
-          image.at<cv::Vec4b>(y, x)[3] = point.a;
-        }
+    pcl::ConstCloudIterator<PointT> pointIter(*organized_cloud);
+    pcl::CentroidPoint<PointT> blueCentroid, redCentroid, greenCentroid;
+    PointT point;
+    for(int i = pointIter.size(); pointIter.getCurrentIndex() < i; pointIter++) {
+      // Points not within the specified ranges in x, y, and z are considered to be noise and we ignore them
+      if(!(pointIter->x < 0.2 && pointIter->y > 0 && pointIter->z > 0)) continue;
+
+      uint8_t r = pointIter->r;
+      uint8_t g = pointIter->g;
+      uint8_t b = pointIter->b;
+
+      // For the non-noise points we add them to a centroid of the dominant color in that point
+      // This excludes all grey points, which are irrelevant
+      if(b > r && b > g) {
+        blueCentroid.add(*pointIter);
+      } else if(r > b && r > g) {
+        redCentroid.add(*pointIter);
+      } else if(g > b && g > r) {
+        greenCentroid.add(*pointIter);
       }
     }
 
-    showImage(image,"BGR Image");
+    blueCentroid.get(point);
 
-    // Convert the images to binary color threshold images using
-    // BGR values indicating a range of blues, greens, and reds that we want to detect
+    geometry_msgs::Pose2D goal;
+    goal.x = point.x;
+    goal.y = point.y;
+    goal.theta = 0;
+    pose_pub.publish(goal);    
 
-    cv::inRange(image, cv::Scalar(0, 0, 200), cv::Scalar(50, 50, 255), redMask);
-    showImage(redMask, "Red-filtered Image");
+    ROS_INFO_STREAM("Centroid of points: " << point);
 
-    cv::inRange(image, cv::Scalar(200, 0, 0), cv::Scalar(255, 50, 50), blueMask);
-    showImage(blueMask, "Blue-filtered Image");
-
-    cv::inRange(image, cv::Scalar(0, 200, 0), cv::Scalar(50, 255, 50), greenMask);
-    showImage(greenMask, "Green-filtered Image");
-
-    // Compute centroids of each color object -- this will not work if multiple objects of the same color are visible
-    // In that case we can find contours of each object and compute the centroids respectively
-    // Use https://www.learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/
-
-    cv::Moments blueMoments = moments(blueMask, true);
-
-    cv::Point p(blueMoments.m10/blueMoments.m00, blueMoments.m01/blueMoments.m00);
-
-    // Plot a red circle on the blue centroid for visualization purposes
-    circle(image, p, 1, CV_RGB(255,0,0), 3);
-    showImage(image, "Blue Centroid");
-
-  }
-
-  // const geometry_msgs::Pose2D goal
-
-  spinner.stop();
-
-// *************************************************************
-// Image color filtering code below
-// *************************************************************
-/*
-  status = imageHandler->getImage(image);
-
-  // Get a point cloud from the stereo vision system
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
-  sensor_msgs::PointCloud2 rosCloud;
-  // tough_perception::StereoPointCloud cloud;
-  pcStatus = pcHandler.giveStereoCloud(cloud);
-
-  ROS_INFO("point cloud status %s",pcStatus ? "true" : "false");
-  if(pcStatus) {
-    pcl::toROSMsg(*cloud, rosCloud);
-    pcPub.publish(rosCloud);
-  }
-  
-  ROS_INFO_STREAM("[Height]" << imageHandler.giveHeight() << " [width]" <<imageHandler.giveWidth());
-  ROS_INFO("image status %s",status ? "true" : "false");
-
-  if(1 == 0) {
-    showImage(image,"BGR Image");
-
-    // Convert the images to binary color threshold images using
-    // BGR values indicating a range of blues, greens, and reds that we want to detect
-
-    cv::inRange(image, cv::Scalar(0, 0, 200), cv::Scalar(50, 50, 255), redMask);
-    showImage(redMask, "Red-filtered Image");
-
-    cv::inRange(image, cv::Scalar(200, 0, 0), cv::Scalar(255, 50, 50), blueMask);
-    showImage(blueMask, "Blue-filtered Image");
-
-    cv::inRange(image, cv::Scalar(0, 200, 0), cv::Scalar(50, 255, 50), greenMask);
-    showImage(greenMask, "Green-filtered Image");
-
-    // Compute centroids of each color object -- this will not work if multiple objects of the same color are visible
-    // In that case we can find contours of each object and compute the centroids respectively
-    // Use https://www.learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/
-
-    cv::Moments blueMoments = moments(blueMask, true);
-
-    cv::Point p(blueMoments.m10/blueMoments.m00, blueMoments.m01/blueMoments.m00);
-
-    // Plot a red circle on the blue centroid for visualization purposes
-    circle(image, p, 1, CV_RGB(255,0,0), 3);
-    showImage(image, "Blue Centroid");
-
-    // TODO: Use LIDAR or stereo to detect depth for determining 3D location for walking target
-  }
-*/
-  return 0;
+    spinner.stop();
+    return 0;
 }
